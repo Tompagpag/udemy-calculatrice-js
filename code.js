86b19ef4e1a2439d9c00a95d87ba661479041712
Tom Paganel <paganel.tom@gmail.com>
2022-02-09 12:15:08 +0100

    create index and code.js

---
+++ code.js
@@ -0,0 +1,7 @@
+do {
+  var choice = parseInt(prompt("Que souhaitez-vous faire ?\n\n 1 - Addition\n 2 - Multiplication\n 3 - Soustraction\n 4 - Division\n"));
+} while (choice != 1 && choice != 2 && choice != 3 && choice != 4);
+
+do {
+
+} while (condition);
