a66635f46656512bfc396beb7d7e8a3bcab6c3b8
Tom Paganel <paganel.tom@gmail.com>
2022-02-10 09:58:38 +0100

    first part done

--- code.js
+++ code.js
@@ -1,7 +1,22 @@
 do {
+
   var choice = parseInt(prompt("Que souhaitez-vous faire ?\n\n 1 - Addition\n 2 - Multiplication\n 3 - Soustraction\n 4 - Division\n"));
+
 } while (choice != 1 && choice != 2 && choice != 3 && choice != 4);
 
 do {
 
-} while (condition);
+  var firstNumber = parseInt(prompt("Quel est le premier nombre ?"));
+  console.log(typeof firstNumber);
+} while (isNaN(firstNumber));
+
+do {
+
+  var secondNumber = parseInt(prompt("Quel est le deuxième nombre ?"));
+
+} while (isNaN(secondNumber));
+
+
+function addition(nombreA, nombreB) {
+  return nombreA + nombreB
+}
